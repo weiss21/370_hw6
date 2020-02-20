4f12708ad71af248ba4df3e734fc9a3652e47eef
weiss21 <waisalirobleh@gmail.com>
2020-02-20 09:31:41 +0000

    start

---
+++ heap.cpp
@@ -0,0 +1,26 @@
+/*
+ * Title: heap.cpp
+ * Abstract:  When the program
+starts, it should read a set of numbers from a user and store them in an array in the
+order the numbers are entered. 
+ * Author: Wais Robleh
+ * ID: 1624
+ * Date: 02/22/2020
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+    int inSize;
+    cout << "Input Size: ";
+    cin >> inSize;
+    cout << endl;
+    
+    vector<int> heap;
+    
+    return 0;
+}
