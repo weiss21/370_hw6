5fd04458674ec51b98096be799007685e27278b4
weiss21 <waisalirobleh@gmail.com>
2020-02-21 07:45:04 +0000

    check heap

--- heap.cpp
+++ heap.cpp
@@ -1,8 +1,7 @@
 /*
  * Title: heap.cpp
- * Abstract:  When the program
-starts, it should read a set of numbers from a user and store them in an array in the
-order the numbers are entered. 
+ * Abstract:  When the program starts, it should read a set of numbers from a 
+ * user and store them in an array in the order the numbers are entered. 
  * Author: Wais Robleh
  * ID: 1624
  * Date: 02/22/2020
@@ -12,15 +11,54 @@ order the numbers are entered.
 
 using namespace std;
 
+bool checkHeap(vector<int> arr, int size){
+    int left, right;
+    
+    for(int i = size/2; i >= 1; i--){
+        left = 2 * i;
+        right = 2 * i + 1;
+        if(left == size){
+            right = 0;
+        } else if (right > size){
+            left = 0;
+            right = 0;
+        }
+        
+        if (arr[left] > arr[i] || arr[right] > arr[i]){
+            return false;
+        }
+    }
+    
+    return true;
+    
+}
 
 int main() {
 
-    int inSize;
+    int inSize, num;
     cout << "Input Size: ";
     cin >> inSize;
     cout << endl;
     
-    vector<int> heap;
+    vector<int> heap(inSize+1,0);
+    
+    cout << "Enter numbers:";
+
+    for (int i = 1; i <= inSize; i++)
+    {
+        cin >> num;
+        heap[i] = num;
+    }
+    
+    if(checkHeap(heap, inSize))
+    {
+        cout << "This is a heap.\n";
+    }
+    
+    for (int i = 1; i <= inSize; i++)
+    {
+        cout << heap[i] << endl;
+    }
     
     return 0;
 }
